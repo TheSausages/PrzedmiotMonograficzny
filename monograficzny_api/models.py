51230bdf86a8de127f6b1683cb03e1595dde5b38
kziejlo <kziejlo@gmail.com>
2023-03-22 21:01:22 +0100

    cleaup

--- monograficzny_api/models.py
+++ monograficzny_api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import datetime
+from sundata import Position
+
 
 # class UsageRequest(models.Model):
 #     power = models.IntegerField()
@@ -9,3 +12,23 @@ from django.db import models
 #
 #     class Meta:
 #         managed = False
+
+# DTO for api call
+class UsageRequest:
+    def __init__(self, power, latitude, longitude, start_date, end_date):
+        self.power = power
+        self.latitude = latitude
+        self.longitude = longitude
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def get_position(self):
+        return Position(self.latitude, self.longitude)
+
+    def get_start_date_as_datetime(self):
+        return datetime.strptime(self.start_date, "%d-%m-%Y")
+
+    def get_range_dates(self):
+        start = datetime.strptime(self.start_date, "%d-%m-%Y").date()
+        end = datetime.strptime(self.end_date, "%d-%m-%Y").date()
+        return range((end - start).days + 1)
--- monograficzny_api/views.py
+++ monograficzny_api/views.py
@@ -1,26 +1,9 @@
-from datetime import timedelta, datetime
+from datetime import timedelta
 
 from django.http import HttpResponse
-from sundata import Position, SunData
+from sundata import SunData
 
-class UsageRequest:
-    def __init__(self, power, latitude, longitude, start_date, end_date):
-        self.power = power
-        self.latitude = latitude
-        self.longitude = longitude
-        self.start_date = start_date
-        self.end_date = end_date
-
-    def get_position(self):
-        return Position(self.latitude, self.longitude)
-
-    def get_start_date_as_datetime(self):
-        return datetime.strptime(self.start_date, "%d-%m-%Y")
-
-    def get_range_dates(self):
-        start = datetime.strptime(self.start_date, "%d-%m-%Y").date()
-        end = datetime.strptime(self.end_date, "%d-%m-%Y").date()
-        return range((end - start).days + 1)
+from monograficzny_api.models import UsageRequest
 
 
 def usage_request(request):
