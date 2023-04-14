6ef5d77be60bf1e5e0f305d7f39437e8170fe2aa
KubaMaciazek <politechnix98@gmail.com>
2023-04-14 15:58:35 +0200

    Radar working

--- frontend/src/Components/Content/subcomponents/Measurement.tsx
+++ frontend/src/Components/Content/subcomponents/Measurement.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Radar from 'radar-sdk-js';
 
 export interface MeasurementProps
 {
@@ -6,6 +7,86 @@ export interface MeasurementProps
 }
 
 const Measurement = (props: MeasurementProps) => {
+    const publishableKey = "prj_test_pk_53339be127338176200d5ee01140449ca09219b4";
+
+  // Radar.initialize(publishableKey);
+  Radar.initialize(publishableKey, {
+    cacheLocationMinutes: 30
+  });
+
+  Radar.trackOnce(function(err: any, result: any) {
+    if (!err) {
+      // do something with result.location, result.events, result.user
+      
+      // ZWRACA BIERZACA LOKALIZACJE I ŚLE DO SERWERA...?
+      console.log(result);
+    }
+
+//     Radar.getLocation(function(err, result) {
+//     if (!err) {
+//       // do something with result.location
+      
+//       // ZWRACA BIERZACA LOKALIZACJE
+//       console.log(result);
+//     }
+//   });
+
+//   // 20 jay st brooklyn ny
+//   Radar.geocode({query: 'sołtysia 23c katowice'}, function(err, result) {
+//     console.log('geocode adress')
+//     if (!err) {
+//       // do something with result.addresses
+
+//       console.log(result);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+
+//   Radar.reverseGeocode({
+//     latitude: 40.783826,
+//     longitude: -73.975363
+//   }, function(err, result) {
+//     console.log('reverse geocode latitude and longitude')
+//     if (!err) {
+//       // do something with result.addresses
+
+//       console.log(result);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+
+//   Radar.ipGeocode(function(err, result) {
+//     console.log('reverse geocode ip')
+//     if (!err) {
+//       // do something with result.address
+
+//       console.log(result);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+
+//   Radar.autocomplete({
+//     query: 'krakow',
+//     near: {
+//       latitude: 40.783826,
+//       longitude: -73.975363
+//     },
+//     limit: 10
+//   }, function(err, result) {
+//     console.log('autocomplete adress')
+//     if (!err) {
+//       // do something with result.addresses
+
+//       console.log(result);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+  });
+
     return (
         <div>
             Measurement
--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="react-scripts" />
+declare module 'radar-sdk-js'
