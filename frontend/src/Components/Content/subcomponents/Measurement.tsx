47b5b8419387729445448537d6e80b3b4041f7c1
KubaMaciazek <politechnix98@gmail.com>
2023-04-17 18:07:31 +0200

    get and set initial adress and location

--- frontend/src/Components/Content/subcomponents/Measurement.tsx
+++ frontend/src/Components/Content/subcomponents/Measurement.tsx
@@ -25,6 +25,7 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 
 const MAPBOX_TOKEN = "pk.eyJ1IjoicG9saXRlY2huaXg5OCIsImEiOiJjbGZ4cnMxNmowdHgxM3FvM2NodndtaHdiIn0.FwaEG3xKV59iMN9zgW-B7A"
+const RADAR_PUBLISHABLE_KEY = "prj_test_pk_53339be127338176200d5ee01140449ca09219b4";
 
 export interface MeasurementProps
 {
@@ -32,30 +33,103 @@ export interface MeasurementProps
 }
 
 const Measurement = (props: MeasurementProps) => {
+    // form
+    const [inputs, setInputs] = useState({
+        address: '',
+        longitude: 0,
+        latitude: 0,
+        start: null,
+        end: null,
+        nrOfLamps: null,
+        singleLampPower: null,
+        observedPowerUsage: null
+    });
+
+    const handleChange = (event: any) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+      }
+
+    const handleStartDateChange = (date: any, event: any) => {
+        //date is a complex object, $d contains Date in format: Wed Feb 04 1998 21:51:31 GMT+0100 (czas środkowoeuropejski standardowy)
+        let value = date.$d.toLocaleDateString()
+        setInputs(values => ({...values, 'start': value}))
+    };
+
+    const handleEndDateChange = (date: any) => {
+        let value = date.$d.toLocaleDateString()
+        setInputs(values => ({...values, 'end': value}))
+    };
+
+    // adress input
     const [inputTypeAdress, setInputType] = React.useState(true);
+    const handleLocationInputChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newInputType: string,
+      ) => {
+          setInputType(!inputTypeAdress);
+      };
+
+    // initial map parameters
     const [viewport, setViewport] = useState({
         latitude: 0,
         longitude: 0,
         zoom: 1,
         transitionDuration: 100,
       });
-    useEffect(() => {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        setViewport({
-          ...viewport,
-          latitude: pos.coords.latitude,
-          longitude: pos.coords.longitude,
-          zoom: 3.5,
-        });
-      });
-    });
 
-    const handleLocationInputChange = (
-      event: React.MouseEvent<HTMLElement>,
-      newInputType: string,
-    ) => {
-        setInputType(!inputTypeAdress);
-    };
+    //compute initial location and adress
+    Radar.initialize(RADAR_PUBLISHABLE_KEY);
+
+    const geocode = (address: string) => {}
+
+    const reverseGeocode = (latitude: number, longitude: number) => {
+        Radar.reverseGeocode({
+            latitude: latitude,
+            longitude: longitude
+        }, function(err: any, result: any) {
+            if (!err) {
+                console.log(result)
+                setInputs({...inputs, 'address': result.addresses[0].formattedAddress});
+            } else {
+                console.log(err);
+            }
+        });       
+    }
+
+    //get initial location
+
+    useEffect(() => {
+        Radar.trackOnce(function(err: any, result: any) {
+            console.log('Get initial location and adress')
+            if (!err) {
+                console.log(result);
+                reverseGeocode(result.location.latitude, result.location.longitude);
+            } else {
+                console.error(err);
+            }
+        });
+    }, []); //HOW TO PROPERLY CALL IT ONCE???
+
+
+    // get initial map
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          setViewport({
+            ...viewport,
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            zoom: 3.5,
+          });
+  
+          setInputs({
+              ...inputs,
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude
+          });
+        });
+    });
 
     return (
         <div>
@@ -92,29 +166,39 @@ const Measurement = (props: MeasurementProps) => {
                         </ToggleButtonGroup>
                         <TextField
                             id="addres"
+                            name='address'
                             label="Adres"
                             variant="outlined"
                             disabled={!inputTypeAdress}
+                            value={inputs.address}
+                            onChange={handleChange}
                         />
                         <TextField
                             id="longitude"
+                            name='longitude'
                             label="Długość geograficzna"
                             variant="outlined"
                             disabled={inputTypeAdress}
+                            value={inputs.longitude}
+                            onChange={handleChange}
                         />
                         <TextField
                             id="latitude"
+                            name='latitude'
                             label="Szerokość geograficzna"
                             variant="outlined"
                             disabled={inputTypeAdress}
+                            value={inputs.latitude}
+                            onChange={handleChange}
                         />
                         <Typography variant="h5" color="text.secondary" component="p">
                             Bierząca wybrana lokalizacja
                         </Typography>
+
+                        {/* ------------------- MAPA ----------------------- */}
                         <div className="outerWrapper">
-                        {viewport.latitude && viewport.longitude && (
+                        {inputs.latitude && inputs.longitude && (
                             <div className="myMapWrapper">
-                            {/* <h5>Your Location:</h5> */}
                                 <Map
                                     mapboxAccessToken={MAPBOX_TOKEN}
                                     initialViewState={viewport}
@@ -122,8 +206,8 @@ const Measurement = (props: MeasurementProps) => {
                                     // class="myMap"
                                 >
                                     <Marker
-                                    longitude={viewport.longitude}
-                                    latitude={viewport.latitude}
+                                    longitude={inputs.longitude}
+                                    latitude={inputs.latitude}
                                     />
                                 </Map>
                             </div>
@@ -137,8 +221,17 @@ const Measurement = (props: MeasurementProps) => {
                             II. Okres pomiaru
                         </Typography>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DatePicker label="Początek pomiaru" />
-                            <DatePicker label="Koniec pomiaru" />
+                            <DatePicker
+                                label="Początek pomiaru"
+                                // format="DD/MM/YYYY"
+                                value={inputs.start}
+                                onChange={handleStartDateChange}
+                            />
+                            <DatePicker
+                                label="Koniec pomiaru"
+                                value={inputs.end}
+                                onChange={handleEndDateChange}
+                            />
                         </LocalizationProvider>
 
                         <Typography variant="h5" color="text.secondary" component="p">
@@ -146,31 +239,45 @@ const Measurement = (props: MeasurementProps) => {
                         </Typography>
                         <TextField
                             id="nrOfLamps"
+                            name='nrOfLamps'
                             label="Ilość lamp w instalacji"
                             variant="outlined"
                             type="number"
                             InputLabelProps={{ shrink: true }}
+                            value={inputs.nrOfLamps}
+                            onChange={handleChange}
                         />
                         <TextField
-                            id="sinleLampPower"
+                            id="singleLampPower"
+                            name='singleLampPower'
                             label="Moc pojednyńczej lampy"
                             variant="outlined"
                             type="number"
+                            InputLabelProps={{ shrink: true }}
+                            value={inputs.singleLampPower}
+                            onChange={handleChange}
                         />
 
                         {/* const [name, setName] = React.useState('Cat in the Hat');                        <TextField */}
                         <TextField
                             id="observedPowerUsage"
+                            name='observedPowerUsage'
                             label="Zaobserwowane rzeczywiste zużycie danych w danym okresie"
                             variant="outlined"
                             type="number"
-                            // value={name}
+                            InputLabelProps={{ shrink: true }}
+                            value={inputs.observedPowerUsage}
+                            onChange={handleChange}
                             // onChange={(event) => {
                             //     setName(event.target.value)
                             // }}
                         />
 
-                        <Button variant="contained" endIcon={<SendIcon />}>
+                        <Button
+                            variant="contained"
+                            onClick={(e) => {console.log(inputs)}}
+                            endIcon={<SendIcon />}
+                        >
                             Przeprowadź analizę
                         </Button> 
                     </Stack>
